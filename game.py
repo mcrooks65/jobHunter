5255080d0853e17816f4dc1eb037d7ae73b3eeec
mcrooks65 <mfcrooks@gmail.com>
2022-02-10 18:17:18 -0800

    Create game.py w/ welcome

---
+++ game.py
@@ -0,0 +1,8 @@
+def main():
+    print("Welcome to Job Hunter v0.1")
+    player_name = input("What's your name? > ")
+    print("Welcome " + player_name + "! Time to carpe some diem.") 
+    
+
+if __name__ == '__main__':
+    main()
