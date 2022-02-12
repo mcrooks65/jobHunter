467673c3fc32f255efff09965ad8c408a3a2bfa0
mcrooks65 <mfcrooks@gmail.com>
2022-02-11 19:06:51 -0800

    Python module test

---
+++ file_one.py
@@ -0,0 +1,8 @@
+# Python module test file_one
+import file_two
+
+def main():
+    print("File one __name__ is set to: {}" .format(__name__))
+
+if __name__ == '__main__':
+    main()
---
+++ file_two.py
@@ -0,0 +1,3 @@
+# Python module to import
+
+print("File two __name__ is set to: {}" .format(__name__))
