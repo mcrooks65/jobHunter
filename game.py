8ae5f22cc22949acf0e7012e0fdab0ef212b5142
mcrooks65 <mfcrooks@gmail.com>
2022-02-11 19:22:14 -0800

    Add time delay between prompts

--- game.py
+++ game.py
@@ -1,8 +1,12 @@
+import time
+
 def main():
     print("Welcome to Job Hunter v0.1")
+    time.sleep(1)
     player_name = input("What's your name? > ")
     print("Welcome to Job Hunter " + player_name + "!  It's time to carpe some diem!") 
-    print("THE RULES OF Job Hunter: To win Job Hunter all you need to do is accept an offer to work at any company!  Start by sending out applications, then work your way through numerous stages of interviews until you get a job offer!  Not a good enough offer?  Get a higher end game score with by securing a better job!")
+    time.sleep(2)
+    print("THE RULES OF Job Hunter: To win Job Hunter all you need to do is accept an offer to work at any company!  Start by sending out applications, then work your way through numerous stages of interviews until you get a job offer!  Not a good enough offer?  Get a higher end game score by securing a better job!")
 
 
 if __name__ == '__main__':
