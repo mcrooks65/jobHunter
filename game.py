437b030caf70a7f263b7e40e783fa7fed840141b
mcrooks65 <mfcrooks@gmail.com>
2022-02-11 19:19:05 -0800

    Add rules display

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 def main():
     print("Welcome to Job Hunter v0.1")
     player_name = input("What's your name? > ")
-    print("Welcome to Job Hunter " + player_name + "! Time to carpe some diem.") 
+    print("Welcome to Job Hunter " + player_name + "!  It's time to carpe some diem!") 
+    print("THE RULES OF Job Hunter: To win Job Hunter all you need to do is accept an offer to work at any company!  Start by sending out applications, then work your way through numerous stages of interviews until you get a job offer!  Not a good enough offer?  Get a higher end game score with by securing a better job!")
+
 
 if __name__ == '__main__':
     main()
