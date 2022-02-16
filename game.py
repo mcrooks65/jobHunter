06271b5a35bf96218c36330bf86ed331041dfa3d
mcrooks65 <mfcrooks@gmail.com>
2022-02-15 20:50:17 -0800

    Begin work on player_choice tree and responses

--- game.py
+++ game.py
@@ -10,22 +10,55 @@ def main():
     print("Not a good enough offer?  Get a higher end game score by securing a better job!")
 
     #Initialize date and several stats
-    date = "Day 1"
+    date = 1
     player_energy = 100 
     player_stress = 0
+    application_count = 0
 
-    print(date)
-    print("Energy: " + str(player_energy))
-    print("Stress: " + str(player_stress)) 
+    def display_stats():
+        print("Day " + str(date))
+        print("Energy: " + str(player_energy))
+        print("Stress: " + str(player_stress)) 
 
-    print("What will you do?")
-    print("1) Check Email (Does not cost energy)")
-    print("2) Apply to a job (Cost: 20 energy 5 Stress)")
-    print("3) Cultural Interview Practice (UNDER CONSTRUCTION)")
-    print("4) Technical Interview Practice (UNDER CONSTRUCTION)")
-    print("5) Vidya Games (UNDER CONSTRUCTION)")
-    print("6) Sleep (Go to next Day, recover 100 energy (maxes at 100), relieve 25 stress")
-    player_input = input("Select your next action: ")
+    def print_choices():
+        print("What will you do?")
+        print("1) Check Email (Does not cost energy)")
+        print("2) Apply to a job (Cost: 20 energy 5 Stress)")
+        print("3) Cultural Interview Practice (UNDER CONSTRUCTION)")
+        print("4) Technical Interview Practice (UNDER CONSTRUCTION)")
+        print("5) Vidya Games (UNDER CONSTRUCTION)")
+        print("6) Sleep (Go to next Day, recover 100 energy (maxes at 100), relieve 25 stress")
+        player_input = input("Select your next action: ")
+        
+        if player_input == "1":
+            print("You have no messages from potential employers.  Better get more applications out!")
+        elif player_input == "2":
+            print("Browsing around on Indeed.com you find a developer postion that ought to fit and go through the applicaiton process")
+            #global application_count
+            #application_count = application_count + 1
+            #player_energy = player_energy - 20
+            #player_stress = player_stress + 5
+        elif player_input == "3":
+            print("UNDER CONSTRUCTION")
+        elif player_input == "4":
+            print("UNDER CONSTRUCTION")
+        elif player_input == "5":
+            print("UNDER CONSTRUCTION")
+        elif player_input == "6":
+            print("You go to bed and rest up for another day of job hunting.")
+            #player_energy = 100
+            #player_stress = player_stress - 25
+            #global date
+            #date += 1
+        else:
+            print("Okay fine bye then!")
+
+    display_stats()
+    print_choices()
+    
+
+
+
     
 if __name__ == '__main__':
     main()
